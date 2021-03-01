b28f1923dba76e0ccd5f56b663d2da5d505d5002
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-01 23:00:07 +0530

    Added: Store the result in dictionary for every computation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -6,13 +6,18 @@ read a b c
 echo The Number are :: $a $b $c
 
 result1=$(($a+$b*$c))
-echo $result1
 
 result2=$(($a*$b+$c))
-echo $result2
 
 result3=$(($c+$a/$b))
-echo $result3
 
 result4=$(($a%$b+$c))
-echo $result4
+
+declare -A ar
+
+ar[UC1]=$result1
+ar[UC2]=$result2
+ar[UC3]=$result3
+ar[UC4]=$result4
+
+echo ${ar[*]}
