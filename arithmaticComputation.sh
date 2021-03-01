052cf0b4e79ebeb337982ae1d1daf067aa04772d
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-01 21:55:55 +0530

    Added: fourth arthmatic Compute (c+a/b)

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,8 +3,5 @@
 echo "enter the numbers for arithmetic computation"
 read a b c
 
-#result1=$(($a+$b*$c))
-#echo $result1
-
-result2=$(($a*$b+$c))
-echo $result2
+result3=$(($c+$a/$b))
+echo $result3
