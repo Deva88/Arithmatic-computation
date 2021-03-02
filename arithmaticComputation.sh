a67e5afd87538faedb11fc7f798c2c1046825dd5
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-02 21:09:03 +0530

    Added: Read the value from the Dictionary into the array

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,23 +1,34 @@
 #!/bin/bash -x
 
 echo "enter the numbers for arithmetic computation"
+#TO TAKING INPUT FROM USER
 read a b c
 
-echo The Number are :: $a $b $c
-
+#COMPUTE ARITHMETIC OPERATION
 result1=$(($a+$b*$c))
-
+echo "result :" $result1
 result2=$(($a*$b+$c))
-
+echo "result :" $result2
 result3=$(($c+$a/$b))
-
+echo "result :" $result3
 result4=$(($a%$b+$c))
+echo "result :" $result4
 
+#TO DECLEAR THE DICTIONARY
 declare -A ar
 
-ar[UC1]=$result1
-ar[UC2]=$result2
-ar[UC3]=$result3
-ar[UC4]=$result4
+#TO STORE THE DICTIONARY
+ar[result1]=$result1
+ar[result2]=$result2
+ar[result3]=$result3
+ar[result4]=$result4
 
-echo ${ar[*]}
+#TO READ THE DICTIONARY VALUE IN ARRAY
+for((i=0; i<=${#ar[@]}; i++))
+do
+
+array[i]=${ar[result$((i+1))]}
+done
+#to print all
+
+echo " ${array[@]} "
